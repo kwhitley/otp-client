2570bb7e0595cce3e6f2ee14597b955bd8cd56ae
Kevin Whitley <krwhitley@gmail.com>
2023-01-11 22:16:58 -0600

    released v0.1.6 - prevent unlock page shift

--- src/routes/(misc)/unlocked/+page.svelte
+++ src/routes/(misc)/unlocked/+page.svelte
@@ -5,7 +5,7 @@
   import '~/styles/app.scss'
 
   let countdown = 10
-  let timer
+  let timer = true
 
   const cancelTimer = (e) => {
     e.preventDefault()
@@ -35,6 +35,22 @@
 <main>
   <h1>Success!</h1>
 
+  <p>
+    You should now be logged in to <strong class="app-name">test</strong> in your original window, and this page may be closed at
+    any time. Thanks for using OTP Garden!
+  </p>
+
+  {#if timer}
+    <p out:slide>This window will close in {countdown} seconds... <a href="/" on:click={cancelTimer}>(cancel)</a></p>
+  {/if}
+
+  <section class="details">
+    <p>OTP Garden is trying to solve the authentication experience for developers and users alike.
+      Did you like this experience?  If so, consider helping out the team by sharing your thoughts
+      on <a href="https://twitter.com/intent/tweet?text=I'm%20loving%20%40otpgarden!">Twitter</a>.
+      Your feedback (and the exposure) helps us greatly!</p>
+  </section>
+
   <section class="confetti-origin">
     <Confetti
       amount={500}
@@ -50,25 +66,8 @@
       fallDistance="5vh"
       />
   </section>
-
-  <p>
-    You should now be logged in to <strong class="app-name">test</strong> in your original window, and this page may be closed at
-    any time.
-
-    <br /><br />Thanks for using OTP Garden!
-  </p>
-
-  {#if timer}
-    <p out:slide>This window will close in {countdown} seconds... <a href="/" on:click={cancelTimer}>(cancel)</a></p>
-  {/if}
-
-  <section class="details">
-    <p>OTP Garden is trying to solve the authentication experience for developers and users alike.
-      Did you like this experience?  If so, consider helping out the team by sharing your thoughts
-      on <a href="https://twitter.com/intent/tweet?text=I'm%20loving%20%40otpgarden!">Twitter</a>.
-      Your feedback (and the exposure) helps us greatly!</p>
-  </section>
 </main>
+
 
 <style lang="scss">
   .app-name {
